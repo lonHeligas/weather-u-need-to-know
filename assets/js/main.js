1a2ae1abc5d22956cc79b198a67f0adbb252a248
Lon Heligas <lon.heligas@gmail.com>
2023-01-14 17:13:22 -0600

    added text to screen. working on icon

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,23 +3,26 @@ const apiKey = '4f2db4d9e71e8d3d6ecabed5a59fb6e5';
 
 
 // ~ the liveVariables don't need to exist, I think.
-// let liveTemperature = $('#live-temperature');
-// let liveWindSpeed = $('#live-windSpeed');
-// let liveHumidity = $('#live-humidity');
-// let liveCity = $('live-city');
+let liveCityEl = $('#live-city');
+let iconEl = $('#icon');
+let liveTempEl = $('#live-temperature');
+let liveWindSpeedEl = $('#live-windSpeed');
+let liveHumidityEl = $('#live-humidity');
 let incomingHistory = localStorage.getItem("cityHistory");
 let incomingCityHihstory = [];
 let userCitySearch = $('#userCityRequest');
 let btnSearch = $('#searchButton');
 let btnCity = $('#cityButton');
 let citySearch = "";
-let todayDate = "";
+
+let dateRaw = moment();
 
 
 
 
 
-// pulls from local storage & checks to see if it's empty
+
+// * pulls from local storage & checks to see if it's empty
 function getCityHistory (){
   try{
     incomingCityHsitory = JSON.parse(incomingCityHistory)
@@ -39,43 +42,79 @@ function getCityHistory (){
 
 
 function getWeatherData(){  
+
   var apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${citySearch}&appid=${apiKey}&units=imperial`
-  // console.log(apiURL);
+  // *console.log(apiURL);
 
   fetch(apiURL)
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
-      // *console.log(data)
+      console.log(data)
   
     // TODO logging out the variables to confirm their existence/layout
-      // *console.log(data.name);
-      // *console.log(data.main.temp);
-      // *console.log(data.wind.speed);
+      // ~ console.log(data.name);
+      // ~ console.log(data.main.temp);
+      // ~ console.log(data.wind.speed);
+
+      // ? establishes the variables for the panel
+
+      var liveCity = data.name;
+      var liveIconId = data.weather[0].icon;
+      var liveTemp = data.main.temp;
+      var liveWind = data.wind.speed;
+      var liveHumid = data.main.humidity;
+
+      const liveIcon = `http://openweathermap.org/img/w/${liveIconId}.png`
+
+      // ~ console.log(liveCity);
+      // ~ console.log(liveTemp);
+      // ~ console.log(liveWind);
+      // ~ console.log(liveHumid);
+      console.log(liveIcon);
+
+      // *posts the information to the DOM
+      liveCityEl.text(liveCity);         
+      liveIconEl = liveIcon;
+          
+      liveTempEl.text(`Temp: ${liveTemp} °F`);
+      liveWindSpeedEl.text(`Wind: ${liveWind} MPH`);
+      liveHumidityEl.text(`Humidity: ${liveHumid}%`);
 
 
-      // $('#live-temperature');
+
+
+
+
+
+
+// $('#live-temperature');
 // $('#live-windSpeed');
 // $('#live-humidity');
 // $('live-city');
-    liveTemperature = $(data.temp);
+
+      // console.log(liveTempEl);
     
     });
 }
 
+// TODO get the 5 day forecast and build out the day cards
+
+// TODO save the city as a button and put it on the left side
+//    TODO save the city data as localstorage information 
 
 
 
-function searchButtonListen(){
-  
+function searchButtonListen(){ 
   
 
   btnSearch.on('click', function(event){
     event.preventDefault();
     citySearch = $('#userCityRequest').val();
-    // console.log(citySearch);
-    // console.log('you clicked search!') ;
+    // *console.log(citySearch);
+    // *console.log('you clicked search!') ;
+    // TODO store the city name and build the button
     getWeatherData()
 
 
